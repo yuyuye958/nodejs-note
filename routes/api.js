a5d1578f34da2660d3c30169662ada769952ef10
Allen <yuyuye958@qq.com>
2019-01-21 19:54:41 +0800

    增加 note 的权限控制

--- routes/api.js
+++ routes/api.js
@@ -4,34 +4,67 @@ var Note = require('../model/note').Note
 
 // 获取所有 notes
 router.get('/notes', (req, res, next) => {
-  Note.findAll({raw: true}).then((notes) => {
+  let query = {raw: true}
+  if (!req.session.user) {
+    query.where = {
+      uid: req.session.user.id
+    }
+  }
+
+  Note.findAll(query).then((notes) => {
     res.send({status: 0, data: notes})
   })
 })
 
 // 创建 note
 router.post('/notes/add', (req, res, next) => {
-  Note.create({text: req.body.note}).then(() => {
+  if (!req.session.user) {
+    return res.send({status: 1, errorMsg: '请先登录'})
+  }
+
+  Note.create({text: req.body.note, uid: req.session.user.id}).then(() => {
     res.send({status: 0})
   }).catch(() => {
-    res.send({status: 1, errorMsg: '数据库发生错误'})
+    res.send({status: 1, errorMsg: '创建数据库发生错误'})
   })
 })
 
 // 编辑 note
 router.post('/notes/edit', (req, res, next) => {
+  if (!req.session.user) {
+    return res.send({status: 1, errorMsg: '请先登录'})
+  }
+
   Note.update(
       {text: req.body.note},
-      {where: {id: req.body.id}}
+      {
+        where: {
+          id: req.body.id,
+          uid: req.session.user.id
+        }
+      }
   ).then(() => {
     res.send({status: 0})
+  }).catch(() => {
+    res.send({status: 1, errorMsg: '修改数据库发生错误'})
   })
 })
 
 // 删除 note
 router.post('/notes/delete', (req, res, next) => {
-  Note.destroy({where: {id: req.body.id}}).then(() => {
+  if (!req.session.user) {
+    return res.send({status: 1, errorMsg: '请先登录'})
+  }
+
+  Note.destroy({
+    where: {
+      id: req.body.id,
+      uid: req.session.user.id
+    }
+  }).then(() => {
     res.send({status: 0})
+  }).catch(() => {
+    res.send({status: 1, errorMsg: '删除数据库发生错误'})
   })
 })
 
